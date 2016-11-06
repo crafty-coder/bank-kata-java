910c3b414019d4b8f77866bfff8983c04f833184
Carlos <carlospb86@gmail.com>
2016-11-06 14:40:36 +0000

    SystemClock Should return todays date in dd mm yyyy

---
+++ src/main/java/org/craftycoder/bankkata/infrasctucture/SystemClock.java
@@ -0,0 +1,23 @@
+package org.craftycoder.bankkata.infrasctucture;
+
+import org.craftycoder.bankkata.ports.Clock;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class SystemClock implements Clock {
+
+    @Override
+    public String today() {
+        LocalDate today = now();
+        return format(today);
+    }
+
+    private String format(LocalDate today) {
+        return today.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
+
+    protected LocalDate now() {
+        return LocalDate.now();
+    }
+}
---
+++ src/test/unit/org/craftycoder/bankkata/infrasctucture/SystemClockShould.java
@@ -0,0 +1,28 @@
+package org.craftycoder.bankkata.infrasctucture;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SystemClockShould {
+
+    @Test public void
+    return_todays_date_in_ddmmyyyy_format(){
+
+        SystemClock clock = new SystemClockFake();
+        String today = clock.today();
+
+        assertThat(today,is("02/12/2016"));
+    }
+
+
+    class SystemClockFake extends SystemClock{
+        @Override
+        protected LocalDate now() {
+            return LocalDate.of(2016,12,2);
+        }
+    }
+}
