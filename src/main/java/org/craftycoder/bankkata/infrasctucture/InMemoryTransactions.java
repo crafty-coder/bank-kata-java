0a94c6d71950399f9b251c5f5c306c93e4e1cac7
Carlos <carlospb86@gmail.com>
2016-11-06 13:29:01 +0000

    Green: InMemoryTransactions Should contain all the transaction registered

--- src/main/java/org/craftycoder/bankkata/infrasctucture/InMemoryTransactions.java
+++ src/main/java/org/craftycoder/bankkata/infrasctucture/InMemoryTransactions.java
@@ -5,21 +5,27 @@ import org.craftycoder.bankkata.ports.Clock;
 import org.craftycoder.bankkata.ports.Transactions;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
 public class InMemoryTransactions implements Transactions {
 
+    private Clock clock;
+    private List<Transaction> storedTransactions;
+
     public InMemoryTransactions(Clock clock) {
+        this.clock = clock;
+        storedTransactions = new ArrayList<>();
     }
 
     @Override
     public void register(int amount) {
-
+        storedTransactions.add(new Transaction(clock.today(),amount));
     }
 
     @Override
     public List<Transaction> findAll() {
-        return new ArrayList<>();
+        return Collections.unmodifiableList(storedTransactions);
     }
 }
