a85f11b80244600c34de121792a0116d2f85f7a4
Carlos <carlospb86@gmail.com>
2016-11-06 13:24:01 +0000

    Red: Add Test InMemoryTransactions Should contain all the transaction register
    -> Fix issue: Test error descriptions where not displayed properly

--- src/test/unit/org/craftycoder/bankkata/infrasctucture/InMemoryTransactionsShould.java
+++ src/test/unit/org/craftycoder/bankkata/infrasctucture/InMemoryTransactionsShould.java
@@ -3,6 +3,7 @@ package org.craftycoder.bankkata.infrasctucture;
 
 import org.craftycoder.bankkata.Transaction;
 import org.craftycoder.bankkata.ports.Clock;
+import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +12,7 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+
 
 public class InMemoryTransactionsShould {
 
@@ -27,11 +29,30 @@ public class InMemoryTransactionsShould {
 
     }
 
-    @Test public void
-    contain_no_transactions_when_created(){
+    @Test
+    public void
+    contain_no_transactions_when_created() {
         List<Transaction> storedTransactions = transactions.findAll();
 
-        assertThat(storedTransactions.size(),is(0));
+        assertThat(storedTransactions.size(), is(0));
+    }
+
+    @Test
+    public void
+    contain_all_the_transactions_registered() {
+
+        context.checking(new Expectations() {{
+            allowing(clock).today();  will(onConsecutiveCalls(returnValue("01/12/2016"),returnValue("02/12/2016")));
+        }});
+
+        transactions.register(100);
+        transactions.register(-100);
+
+        List<Transaction> storedTransactions = transactions.findAll();
+
+        assertThat(storedTransactions.size(), is(2));
+        assertThat(storedTransactions.get(0), is(new Transaction("01/12/2016",100)));
+        assertThat(storedTransactions.get(1), is(new Transaction("02/12/2016",-100)));
     }
 
 }
